6c83d7cba0cb44cb38d04b8723a53b201e5dd49a
fenosoa <hugoandriamaromanana@gmail.com>
2026-07-22 20:04:39 +0300

    [fix] main file

--- kl_expenses/src/wsgi.py
+++ kl_expenses/src/wsgi.py
@@ -4,6 +4,6 @@ from src.config import DATABASE_PATH, LOG_DIR
 from src.app.context import AppContext
 from src.app.create import create_app
 
-_CTX = AppContext.init_app(DATABASE_PATH, LOG_DIR)
-
-APP = create_app(_CTX)
+if __name__ == "__main__":
+    _CTX = AppContext.init_app(DATABASE_PATH, LOG_DIR)
+    APP = create_app(_CTX)
