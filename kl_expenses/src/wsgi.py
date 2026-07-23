b0b0c621194e65f6fd76444d85fccc7256e806bd
fenosoa <hugoandriamaromanana@gmail.com>
2026-07-23 11:56:39 +0300

    [fix] gunicorn endpoint

--- kl_expenses/src/wsgi.py
+++ kl_expenses/src/wsgi.py
@@ -1,9 +1,7 @@
-# app/wsgi.py
-
+# src/wsgi.py
 from src.config import DATABASE_PATH, LOG_DIR
 from src.app.context import AppContext
 from src.app.create import create_app
 
-if __name__ == "__main__":
-    _CTX = AppContext.init_app(DATABASE_PATH, LOG_DIR)
-    APP = create_app(_CTX)
+_CTX = AppContext.init_app(DATABASE_PATH, LOG_DIR)
+APP = create_app(_CTX)
