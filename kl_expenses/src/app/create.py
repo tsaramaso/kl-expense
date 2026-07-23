03290d165d7fad946d674ed2d0035030217e76ba
fenosoa <hugoandriamaromanana@gmail.com>
2026-07-23 11:56:16 +0300

    [fix] template static path

--- kl_expenses/src/app/create.py
+++ kl_expenses/src/app/create.py
@@ -1,14 +1,14 @@
 # app/flask_app.py
 from flask import Flask
 
-from src.config import FORCE_SECURE_COOKIE, SECRET_KEY
+from src.config import FORCE_SECURE_COOKIE, SECRET_KEY, STATIC_FOLDER, TEMPLATE_FOLDER
 from src.app.context import AppContext
 from src.db.session import close_session, open_session
 from src.app.routes import bp
 
 
 def create_app(ctx: AppContext) -> Flask:
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder=TEMPLATE_FOLDER, static_folder=STATIC_FOLDER)
     app.extensions["ctx"] = ctx
 
     app.config["SECRET_KEY"] = SECRET_KEY
